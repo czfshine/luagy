9cf8451c9124f7560a6cd53666ab6b671eee2d3a
chen zefan <czfshine@gmail.com>
2015-06-11 20:19:28 +0800

    黑夜从大地上升起，遮住了光明的天空，丰收后荒凉的大地，黑夜从你内部上升。

---
+++ system/osinfo.lua
@@ -0,0 +1,39 @@
+--- osinfo.lua
+-- get Operating System INFOrmation
+-- @usage:
+-- in other file 
+--~ require "osinfo"
+--~if os.islinux then 
+-- some code 
+--~elseif os.iswindows then 
+-- smoe code
+--~else --other OS like Mac
+-- some code 
+--~end
+
+
+os.iswindows = os.getenv('WINDIR') or (os.getenv('OS') or ''):match('[Ww]indows')
+
+os.islinux = not os.iswindows and not os.getenv('DYLD_LIBRARY_PATH') and io.open("/proc")
+os.arch = "x86" -- use 32bit by default
+unpack = table.unpack or unpack
+
+if os.islinux then
+  local file = io.popen("uname -m")
+  if file then
+    local machine=file:read("*l")
+    local archtype= { x86_64="x64", armv7l="armhf" }
+    print(machine)
+    os.arch = archtype[machine] or "x86"
+    file:close()
+  end
+end
+
+package.cpath = (
+  os.iswindows and 'bin/?.dll;bin/clibs/?.dll;' or
+  os.islinux and ('bin/linux/%s/lib?.so;bin/linux/%s/clibs/?.so;'):format(os.arch,os.arch) or
+  --[[isosx]] 'bin/lib?.dylib;bin/clibs/?.dylib;')
+    .. package.cpath
+
+package.path  = 'lualibs/?.lua;lualibs/?/?.lua;lualibs/?/init.lua;lualibs/?/?/?.lua;lualibs/?/?/init.lua;'
+              .. package.path
