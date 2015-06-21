c50285e3f0a34e1c93f5825345302896ea14a389
chen zefan <czfshine@gmail.com>
2015-06-22 07:58:07 +0800

    死亡不是失去生命，而是走出时间。

---
+++ file/attrdir.lua
@@ -0,0 +1,30 @@
+--A function to iterine all file in a directory.
+require"lfs"
+
+local sep = "/"
+local upper = ".."
+
+function attrdir (path,fn)
+  -- function fn(filename,filepath)
+	for file in lfs.dir(path) do
+		if file ~= "." and file ~= ".." then
+			local p = path..sep..file
+			local attr = lfs.attributes (p)
+			assert (type(attr) == "table")
+			if attr.mode == "directory" then
+				attrdir (p,fn)
+			else --is file 
+        fn(file,p)
+			end
+		end
+	end
+end
+
+--[[  test---
+function fn(filename,filepath)
+  print(filename,filepath)
+end
+
+attrdir (".",fn)
+]]--
+return  attrdir
